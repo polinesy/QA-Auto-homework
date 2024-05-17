7aa62c8ab9294554551106e5a139e6b368daf080
polinasydorenko <uvarova.pl@gmail.com>
2024-05-17 14:18:13 +0300

    auto hw 11

--- src/main/java/hw_1/Task4.java
+++ src/main/java/hw_1/Task4.java
@@ -27,57 +27,62 @@ public class Task4 {
     private static final By searchField = By.xpath("//*[@id='search_query_top']");
     private static final By searchButton = By.xpath("//*[@id='searchbox']/button");
     private static final By firstDressButton = By.xpath("//*[@id='center_column']/ul/li[1]/div/div[2]/h5/a");
-    private static final By secondDressButton = By.xpath("///*[@id='center_column']/ul/li/div/div[2]/div[2]/a");
+    private static final By secondDressButton = By.xpath("//a[contains(text(), 'Faded Short Sleeve T-shirts')]");
     private static final By cartButtonFirst = By.xpath("//*[@id='add_to_cart']/button/span");
-    private static final By continueShoppingButton = By.xpath("////div[4]/span/span");
+    private static final By continueShoppingButton = By.xpath("//span[@title='Continue shopping']");
     private static final By womenButton = By.xpath("//*[@id='block_top_menu']/ul/li[1]/a");
     private static final By cartButtonSecond = By.xpath("//*[@id='header']/div[3]/div/div/div[3]/div/a");
     private static final By sizeElement = By.id("group_1");
 
 
-        public static void main(String[] args) {
-            //вызывыем драйвер, открываем страницу
+        public static void main(String[] args) throws InterruptedException {
             WebDriver driver = DriverInit.setUpDriver();
             driver.get(CurrentStrings.mainUrl);
 
-            //вводим поиск
+
             WebElement searchField = driver.findElement(Locators.searchField);
             searchField.sendKeys(CurrentStrings.textOne);
             WebElement searchButton = driver.findElement(Locators.searchButton);
             searchButton.click();
 
-            //переходим на платье
+
             WebElement firstDressButton = driver.findElement(Locators.firstDressButton);
             firstDressButton.click();
 
-            //селектор размера
+
             WebElement sizeElement = driver.findElement(Locators.sizeElement);
             Select sizeSelect = new Select(sizeElement);
             sizeSelect.selectByVisibleText("M");
 
-            // добавляем в корзину
+
             WebElement addToCartButton = driver.findElement(Locators.cartButtonFirst);
             addToCartButton.click();
+            Thread.sleep(4000);
 
-            // возвращаемся к покупкам
+
             WebElement continueShoppingButton = driver.findElement(Locators.continueShoppingButton);
             continueShoppingButton.click();
 
-            // нажимаем вумен
+
             WebElement womenButton = driver.findElement(Locators.womenButton);
             womenButton.click();
+            Thread.sleep(4000);
 
-            //второй поиск
-            searchField.sendKeys(CurrentStrings.textTwo);
-            searchButton.click();
 
-            //второе платье в корзину
+            WebElement searchFieldAfterNavigation = driver.findElement(Locators.searchField);
+            searchFieldAfterNavigation.sendKeys(CurrentStrings.textTwo);
+            WebElement searchButtonAfterNavigation = driver.findElement(Locators.searchButton);
+            searchButtonAfterNavigation.click();
+            Thread.sleep(4000);
+
+
             WebElement secondDressButton = driver.findElement(Locators.secondDressButton);
             secondDressButton.click();
-            addToCartButton.click();
-            continueShoppingButton.click();
+            //addToCartButton.click();
+            //continueShoppingButton.click(); ---> на момент создания теста у этого товара не было доступных размеров и цветов
+            Thread.sleep(4000);
 
-            //переходим в корзину
+
             WebElement cartButtonSecond = driver.findElement(Locators.cartButtonSecond);
             cartButtonSecond.click();
 
