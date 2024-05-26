d9f4b326427d49e5fd07338d2e13be88ec0321ee
polinasydorenko <uvarova.pl@gmail.com>
2024-05-26 21:15:38 +0300

    auto hw 3

--- src/main/java/hw_3/Hw3Task1.java
+++ src/main/java/hw_3/Hw3Task1.java
@@ -16,13 +16,15 @@ public class Hw3Task1 {
         private static final String alertsDemo = "https://www.hyrtutorials.com/p/alertsdemo.html";
         private static final String w3schools = "https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit";
     }
+
     private static class LocatorsGoogle {
-        private static final By link1 = By.xpath("//*[@id='rso']/div[1]/div/div/div/div[1]/div/div/span/a/h3");
+        private static final By link1 = By.xpath("//h3[@class='LC20lb MBeuO DKV0Md' and contains(text(), 'Create account')]");
         private static final By link2 = By.xpath("//h3[contains(text(), 'AlertsDemo')]");
-        private static final By clearButton =  By.xpath("//*[@id='tsf']/div[1]/div[1]/div[2]/div/div[3]/div[1]/div/span/svg");
+        private static final By clearButton = By.xpath("//span[@class='ExCKkf z1asCe rzyADb']");
         private static final By textArea1 = By.xpath("//textarea[@class='gLFyf' and @id='APjFqb' and @name='q']");
         private static final By textArea2 = By.xpath("//*[@id='APjFqb']");
     }
+
     private static class LocatorsW3Schools {
         private static final By firstNameW3 = By.name("fname");
         private static final By lastNameW3 = By.name("lname");
@@ -30,7 +32,7 @@ public class Hw3Task1 {
         private static final By noteText = By.xpath("/html/body/div[2]/p");
     }
 
-    private static class LocatorsGuinness{
+    private static class LocatorsGuinness {
         private static final By lastNameG = By.id("LastName");
         private static final By firstNameG = By.id("FirstName");
         private static final By birthDay = By.id("DateOfBirthDay");
@@ -42,32 +44,34 @@ public class Hw3Task1 {
         private static final By emailAddressConfirm = By.id("ConfirmEmailAddress");
         private static final By password = By.id("Password");
         private static final By passwordConfirm = By.id("ConfirmPassword");
-        private static final By passwordText = By.xpath("//span[contains(text(), 'Confirm password' and 'Password' do not match.')]");
+        private static final By passwordText = By.xpath("//span[contains(text(), 'do not match.')]");
     }
 
 
-
     // шаг 1 - открываем все вкладки
-    public static void openAllPages (WebDriver driver) {
+    public static void openAllPages(WebDriver driver) throws InterruptedException {
         Actions actions = new Actions(driver);
         driver.get(Urls.googleSearch);
         WebElement textArea1 = driver.findElement(LocatorsGoogle.textArea1);
-        WebElement textArea2 = driver.findElement(LocatorsGoogle.textArea2);
-        WebElement link1 = driver.findElement(LocatorsGoogle.link1);
-        WebElement link2 = driver.findElement(LocatorsGoogle.link2);
         //ищем по ссылке
         actions.sendKeys(textArea1, Urls.guinnessPage).sendKeys(Keys.ENTER).perform();
-
-        // открываем вкладку 1 в новом окне
+        Thread.sleep(2000);
+        //добавляем новые элементы на текущей странице
+        WebElement link1 = driver.findElement(LocatorsGoogle.link1);
+        //открываем вкладку 1 в новом окне
         if (System.getProperty("os.name").toLowerCase().contains("mac")) {
             actions.keyDown(Keys.COMMAND).click(link1).keyUp(Keys.COMMAND).perform();
         } else {
             actions.keyDown(Keys.CONTROL).click(link1).keyUp(Keys.CONTROL).perform();
         }
+        //добавляем элементы для второго поиска
+        WebElement textArea2 = driver.findElement(LocatorsGoogle.textArea2);
 
         //ищем по второй ссылке
         driver.findElement(LocatorsGoogle.clearButton).click();
         actions.sendKeys(textArea2, Urls.alertsDemo).sendKeys(Keys.ENTER).perform();
+
+        WebElement link2 = driver.findElement(LocatorsGoogle.link2);
 
         //открываем вторую ссылку в новом окне
         if (System.getProperty("os.name").toLowerCase().contains("mac")) {
@@ -76,29 +80,37 @@ public class Hw3Task1 {
             actions.keyDown(Keys.CONTROL).click(link2).keyUp(Keys.CONTROL).perform();
         }
 
-        driver.get(Urls.w3schools);
     }
 
-   // выполняем действия на странице школы
-    public static void sendName (WebDriver driver) {
 
+
+   // выполняем действия на странице школы
+    public static void sendName (WebDriver driver) throws InterruptedException {
+        driver.get(Urls.w3schools);
+
+        Thread.sleep(3000);
+        /*
         try {
             driver.findElement(By.id("accept-choices")).click();
-        }catch (Exception e){
+        }catch (NoSuchElementException e){
             System.out.println(e.getMessage());
         }
+         */
+
+        WebElement innerIframe = driver.findElement(By.xpath("//iframe[@id='iframeResult']"));
+        driver.switchTo().frame(innerIframe);
 
         WebElement firstName = driver.findElement(LocatorsW3Schools.firstNameW3);
         WebElement lastName = driver.findElement(LocatorsW3Schools.lastNameW3);
         WebElement submitButton = driver.findElement(LocatorsW3Schools.submitButton);
-        WebElement noteText = driver.findElement(LocatorsW3Schools.noteText);
 
-        driver.switchTo().frame("iframeResult");
         firstName.clear();
         firstName.sendKeys("Polina");
         lastName.clear();
         lastName.sendKeys("Sydorenko");
         submitButton.click();
+        Thread.sleep(3000);
+        WebElement noteText = driver.findElement(LocatorsW3Schools.noteText);
         System.out.println(noteText.getText());
         driver.switchTo().defaultContent();
     }
@@ -106,14 +118,16 @@ public class Hw3Task1 {
     // выполняем действия на странице гиннеса
     public static void fillRegistrationForm (WebDriver driver) {
         ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(tabs.get(1));
-
-        try {
-            driver.findElement(By.id("ez-accept-all")).click();
-        }catch (NoSuchElementException e){
-            System.out.println("No cookies button");
+        for (String tab : tabs) {
+            driver.switchTo().window(tab);
+            if (driver.getCurrentUrl().startsWith("https://www.guinnessworldrecords.com")) {
+                try {
+                    driver.findElement(By.id("ez-accept-all")).click();
+                } catch (NoSuchElementException e) {
+                    System.out.println("No cookies button");
+                }
+            }
         }
-
         driver.findElement(LocatorsGuinness.lastNameG).sendKeys("Polina");
         driver.findElement(LocatorsGuinness.firstNameG).sendKeys("Sydorenko");
         driver.findElement(LocatorsGuinness.birthDay).sendKeys("07");
@@ -128,18 +142,62 @@ public class Hw3Task1 {
         driver.findElement(LocatorsGuinness.emailAddressConfirm).sendKeys("mail@mail.cim");
 
         driver.findElement(LocatorsGuinness.password).sendKeys("12345");
+        Actions actions = new Actions(driver);
         driver.findElement(LocatorsGuinness.passwordConfirm).sendKeys("1234567");
+        actions.sendKeys(Keys.ENTER).perform();
 
         System.out.println(driver.findElement(LocatorsGuinness.passwordText).getText());
 
     }
 
-    public static void main(String[] args) {
-        WebDriver driver = DriverInit.setUpDriver();
-        openAllPages(driver);
-        sendName(driver);
-        fillRegistrationForm(driver);
-        driver.quit();
+    //выполняем действия на странице алерт
+public static void alertPage (WebDriver driver){
+    ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+    for (String tab : tabs) {
+        driver.switchTo().window(tab);
+        if (driver.getCurrentUrl().startsWith("https://www.hyrtutorials.com/p/alertsdemo.html")) {
+            try {
+                driver.findElement(By.id("ez-accept-all")).click();
+            } catch (NoSuchElementException e) {
+                System.out.println("No cookies button");
+            }
+        }
     }
+    //клик 1
+    driver.findElement(By.id("alertBox")).click();
+    Alert alert1 = driver.switchTo().alert();
+    alert1.accept();
+    WebElement text1 = driver.findElement(By.id("output"));
+    System.out.println(text1.getText());
+
+    //клик 2
+    driver.findElement(By.id("confirmBox")).click();
+    Alert alert2 = driver.switchTo().alert();
+    alert2.dismiss();
+    WebElement text2 = driver.findElement(By.id("output"));
+    System.out.println(text2.getText());
+
+    //клик 3
+    driver.findElement(By.id("promptBox")).click();
+    Alert alert3 = driver.switchTo().alert();
+    alert3.sendKeys("Final step of this task");
+    alert3.accept();
+    WebElement text3 = driver.findElement(By.id("output"));
+    System.out.println(text3.getText());
 
 }
+
+
+        public static void main (String[]args) throws InterruptedException {
+            WebDriver driver = DriverInit.setUpDriver();
+            openAllPages(driver);
+            //sendName(driver);
+            //fillRegistrationForm(driver);
+            alertPage(driver);
+            driver.quit();
+            //System.out.println("see me");
+        }
+
+}
+
+
