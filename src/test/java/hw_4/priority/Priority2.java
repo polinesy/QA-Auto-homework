07bd9423005d88673868f457e45247d60aeb829e
polinasydorenko <uvarova.pl@gmail.com>
2024-05-29 15:24:31 +0300

    auto hw 4 testGrouping

---
+++ src/test/java/hw_4/priority/Priority2.java
@@ -0,0 +1,39 @@
+package hw_4.priority;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class Priority2 {
+
+    @Test
+    public void a (){
+    assertTrue (true);
+    }
+    @Test
+    public void b (){
+        assertTrue (true);
+    }
+    @Test
+    public void c (){
+        assertTrue (true);
+    }
+    @Test
+    public void d (){
+        assertTrue (true);
+    }
+    @Test
+    public void e (){
+        assertTrue (true);
+    }
+    @Test
+    public void f (){
+        assertTrue (true);
+    }
+    @Test
+    public void g (){
+        assertTrue (true);
+    }
+
+
+}
